04fdaa76cb2cc91846b541cbbcce42dc29f97dff
Denis Makarenko <xhjkl@users.noreply.github.com>
2026-04-25 16:52:36 +0400

    vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+
+export default defineConfig({
+	plugins: [solid()],
+	base: './',
+	build: {
+		target: 'esnext',
+	},
+})
