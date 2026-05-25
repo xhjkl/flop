b2bad877c02340a006a4b3e0fe1f7df137058dc0
Denis Makarenko <xhjkl@users.noreply.github.com>
2026-05-25 19:39:42 +0400

    vite: forward to local proxy in dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import solid from 'vite-plugin-solid'
 export default defineConfig({
 	plugins: [solid()],
 	base: './',
+	server: {
+		proxy: {
+			'/-': {
+				target: 'http://127.0.0.1:8787',
+				ws: true,
+			},
+		},
+	},
 	build: {
 		target: 'esnext',
 	},
